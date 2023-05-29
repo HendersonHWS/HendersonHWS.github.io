870f672519c0fdcf90d19e55ff0acfce7ba2db6f
HendersonHWS <45208622+HendersonHWS@users.noreply.github.com>
2023-05-28 22:23:40 -0400

    add vr button

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
 
 const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
@@ -11,6 +12,8 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+document.body.appendChild( VRButton.createButton( renderer ) );
+renderer.xr.enabled = true;
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -20,12 +23,11 @@ scene.add( cube );
 camera.position.z = 5;
 
 function animate() {
-	requestAnimationFrame( animate );
+	renderer.setAnimationLoop( function () {
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
+        renderer.render( scene, camera );
+    
+    } );
 }
 
 animate();
